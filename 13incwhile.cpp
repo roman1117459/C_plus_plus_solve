ca4c6ebb792ff0bd11cdf54e2ce271679f5f581b
roman1117459 <roman1117459@gmail.com>
2022-12-08 18:52:08 +0600

    feat: while loop done

---
+++ 13incwhile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int num = 1;
+    // while (num < 6){
+    //     cout << "Number: " << num << endl;
+    //     num++;
+    // }
+    int num = 1;
+    int number;
+    
+    while (num <= 5){
+        cin >> number;
+        num++;
+    }
+
+    return 0;
+}
---
+++ 14incprob.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int seconds;
+    cin>>seconds;
+    
+    //your code goes here
+    while (seconds >= 0){
+        cout << seconds << endl;
+        seconds--;
+    }
+    
+    return 0;
+}
